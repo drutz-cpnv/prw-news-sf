9e5654223b018351143fe10f5fedc65b8b6269a9
drutz-cpnv <dimitri.rutz@eduvaud.ch>
2023-12-14 14:33:35 +0100

    Move validation from form to entity

--- src/Entity/Article.php
+++ src/Entity/Article.php
@@ -7,6 +7,7 @@ use Doctrine\Common\Collections\ArrayCollection;
 use Doctrine\Common\Collections\Collection;
 use Doctrine\DBAL\Types\Types;
 use Doctrine\ORM\Mapping as ORM;
+use Symfony\Component\Validator\Constraints as Assert;
 
 #[ORM\Entity(repositoryClass: ArticleRepository::class)]
 class Article
@@ -17,9 +18,13 @@ class Article
     private ?int $id = null;
 
     #[ORM\Column(length: 255)]
+    #[Assert\NotBlank()]
+    #[Assert\Length(min: 5, max: 255)]
     private ?string $title = null;
 
     #[ORM\Column(type: Types::TEXT)]
+    #[Assert\NotBlank()]
+    #[Assert\Length(min: 5)]
     private ?string $body = null;
 
     #[ORM\Column(nullable: true)]
--- src/Form/ArticleForm.php
+++ src/Form/ArticleForm.php
@@ -6,25 +6,14 @@ use App\Entity\Article;
 use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
-use Symfony\Component\Validator\Constraints as Assert;
 
 class ArticleForm extends AbstractType
 {
     public function buildForm(FormBuilderInterface $builder, array $options): void
     {
         $builder
-            ->add('title', null, [
-                'constraints' => [
-                    new Assert\NotBlank(),
-                    new Assert\Length(min: 5, max: 255)
-                ]
-            ])
-            ->add('body', null, [
-                'constraints' => [
-                    new Assert\NotBlank(),
-                    new Assert\Length(min: 5)
-                ]
-            ])
+            ->add('title')
+            ->add('body')
         ;
     }
 
